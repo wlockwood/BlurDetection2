d0e5b7b5dc87fbff7b35c3e111b3ef38466d8edf
wlockwood <jinkside@gmail.com>
2022-04-22 13:00:54 -0700

    Added raw support and denoising.

--- process.py
+++ process.py
@@ -5,19 +5,25 @@ import pathlib
 import json
 
 import cv2
+import rawpy
+import numpy
+from PIL import Image
+from io import BytesIO
 
 from blur_detection import estimate_blur
 from blur_detection import fix_image_size
 from blur_detection import pretty_blur_map
 
+raw_types = ['.nef']
+normal_types = ['.jpg', '.jpeg', '.png']
 
 def parse_args():
     parser = argparse.ArgumentParser(description='run blur detection on a single image')
     parser.add_argument('-i', '--images', type=str, nargs='+', required=True, help='directory of images')
     parser.add_argument('-s', '--save-path', type=str, default=None, help='path to save output')
 
-    parser.add_argument('-tb', '--threshold_blur', type=float, default=50.0, help='blurry threshold')
-    parser.add_argument('-ts', '--threshold_semi', type=float, default=100.0, help='semi-blurry threshold')
+    parser.add_argument('-tb', '--threshold_blur', type=float, default=15.0, help='blurry threshold')
+    parser.add_argument('-ts', '--threshold_semi', type=float, default=25.0, help='semi-blurry threshold')
     parser.add_argument('-f', '--variable-size', action='store_true', help='fix the image size')
 
     parser.add_argument('-v', '--verbose', action='store_true', help='set logging level to debug')
@@ -28,13 +34,14 @@ def parse_args():
     return parser.parse_args()
 
 
-def find_images(image_paths, img_extensions=['.jpg', '.png', '.jpeg']):
+def find_images(image_paths, img_extensions=raw_types + normal_types):
     img_extensions += [i.upper() for i in img_extensions]
 
     for path in image_paths:
         path = pathlib.Path(path)
 
         if path.is_file():
+            logging.info(f"Found image file '{path}'")
             if path.suffix not in img_extensions:
                 logging.info(f'{path.suffix} is not an image extension! skipping {path}')
                 continue
@@ -42,8 +49,33 @@ def find_images(image_paths, img_extensions=['.jpg', '.png', '.jpeg']):
                 yield path
 
         if path.is_dir():
+            logging.info(f"Found directory '{path}'")
             for img_ext in img_extensions:
                 yield from path.rglob(f'*{img_ext}')
+
+
+def load_image(path):
+    is_raw = any(e in path.lower() for e in raw_types)
+    if is_raw:
+        with rawpy.imread(path) as raw:
+            # raises rawpy.LibRawNoThumbnailError if thumbnail missing
+            # raises rawpy.LibRawUnsupportedThumbnailError if unsupported format
+            thumb = raw.extract_thumb()
+
+        if thumb.format == rawpy.ThumbFormat.JPEG:
+            # thumb.data is already in JPEG format, save as-is
+            bio = BytesIO(thumb.data)
+            pilmage = Image.open(bio)
+            out = cv2.cvtColor(numpy.array(pilmage), cv2.COLOR_RGB2BGR)
+
+        elif thumb.format == rawpy.ThumbFormat.BITMAP:
+            #cv2.cvtColor(numpy.array(pilmage), cv2.COLOR_RGB2BGR)
+            out = thumb.data
+    else:
+        out = cv2.imread(str(path))
+    if out is None:
+        raise Exception(f"Failed to load image data from {path}")
+    return out
 
 
 if __name__ == '__main__':
@@ -75,23 +107,31 @@ if __name__ == '__main__':
             logging.warning(f'warning! failed to read image from {image_path}; skipping!')
             continue
 
-        logging.info(f'processing {image_path}')
+        #logging.info(f'processing {image_path}')
 
         if fix_size:
             image = fix_image_size(image)
         else:
             logging.warning('not normalizing image size for consistent scoring!')
 
+        image = cv2.bilateralFilter(image, 5, 75, 75)  # Blur prior to downsampling?
         blur_map, score = estimate_blur(image)
         blurry = bool(score < args.threshold_blur)
         semi_blurry = bool(args.threshold_blur <= score < args.threshold_semi)
+
 
         logging.info(f'image_path: {image_path} score: {score} blurry: {blurry} semi_blurry: {semi_blurry}')
         results.append({'input_path': str(image_path), 'score': score, 'blurry': blurry, 'semi_blurry': semi_blurry})
 
         if args.display:
-            cv2.imshow('input', image)
-            cv2.imshow('result', pretty_blur_map(blur_map))
+            descr = "Blurry" if blurry else "Semi-Blurry" if semi_blurry else "Sharp"
+            text = f"{image_path.name}: {descr}"
+
+            blur_map = pretty_blur_map(blur_map)
+            blur_map = cv2.cvtColor(blur_map, cv2.COLOR_GRAY2BGR)
+            cv2.putText(blur_map, text, (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+            cv2.imshow('result', blur_map)
 
             if cv2.waitKey(0) == ord('q'):
                 logging.info('exiting...')
@@ -101,8 +141,8 @@ if __name__ == '__main__':
             dest_folder = "blurry/" if blurry else "semi_blurry/" if semi_blurry else ""
             if dest_folder:
                 logging.info(f"Pretending to move {image_path} into the {dest_folder}...")
-        else:
-            logging.info(f"Not moving {image_path} as it's not blurry")
+            else:
+                logging.info(f"Not moving {image_path} as it's not blurry")
 
     if save_path is not None:
         logging.info(f'saving json to {save_path}')
