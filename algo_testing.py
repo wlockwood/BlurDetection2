764d711d5ed72010f72a65593bf5066f60fe9523
wlockwood <jinkside@gmail.com>
2022-06-01 14:49:40 -0700

    Refactored for multiprocessing.

--- algo_testing.py
+++ algo_testing.py
@@ -4,8 +4,9 @@ from pathlib import Path
 from enum import Enum, auto
 from typing import Callable
 import cv2
-
-from time import perf_counter as pc
+import concurrent.futures
+from functools import partial
+from time import sleep, perf_counter as pc
 
 from blur_detection.detection import detect_blur_fft, estimate_blur, fix_image_size
 from file_handling import find_images, load_image
@@ -42,70 +43,110 @@ class TestImage:
         return " - ".join(output_elements)
 
     def result_str(self, algorithms: dict[str, Algorithm]):
-        return f"{test_image.path.name:>20} {test_image.expect_result.name:6} --- {self.scores_str(algorithms)}"
+        return f"{self.path.name:>20} {self.expect_result.name:6} --- {self.scores_str(algorithms)}"
 
 
-test_sources = {
-    Sharpness.SHARP: r"C:\Users\jinks\Desktop\Export\blurdet\sharp",
-    Sharpness.BLURRY: r"C:\Users\jinks\Desktop\Export\blurdet\blurry"
-}
-
-image_extensions = ['.jpg', '.jpeg', '.png']
-
-test_images = []
-for src_type, src_path in test_sources.items():
-    image_paths = sorted(list(find_images([src_path])))
-    for i_path in image_paths:
-        test_images.append(TestImage(i_path, src_type))
-print(f"Found {len(test_images):,} test images...")
-
-algorithms = {
-    "Laplace": Algorithm("Laplace", lambda i: estimate_blur(i, just_score=True), 10),
-    "FFT": Algorithm("FFT", detect_blur_fft, 20),
-}
-
-for test_image in test_images:
-    image = load_image(test_image.path)
-    image = fix_image_size(image, 2e6)
-
+def test_an_image(test_im: TestImage, algorithms: dict[str, Algorithm]):
+    image = load_image(test_im.path)
+    image = fix_image_size(image, 3e6)
     for algo in algorithms.values():
         t0 = pc()
         score = algo.call_func(image)
         elapsed = pc() - t0
 
-        test_image.scores[algo.name] = score
-        test_image.times[algo.name] = elapsed
+        test_im.scores[algo.name] = score
+        test_im.times[algo.name] = elapsed
 
         sharp: bool = score >= algorithms[algo.name].blurriness_threshold
         sharpness = Sharpness.SHARP if sharp else Sharpness.BLURRY
-        test_image.actual_results[algo.name] = sharpness
-    score_str = test_image.scores_str(algorithms)
+        test_im.actual_results[algo.name] = sharpness
+    return test_im
 
-    # Suppress hits
-    # miss = any(x != test_image.expect_result for x in test_image.actual_results.values())
-    # if miss:
-    print(f"{test_image.path.name:>20} {test_image.expect_result.name:6} --- {score_str}")
+def print_result(test_im: TestImage, algorithms: dict[str, Algorithm]):
+    print(test_im.result_str(algorithms))
 
-print()
-print(" = Algorithm results = ")
-for algo in algorithms.values():
-    print(f" - {algo.name} - ")
-    total = len(test_images)
-    hits = len([ti for ti in test_images if ti.actual_results[algo.name] == ti.expect_result])
-    misses = total - hits
-    print(f"Hit rate: {hits * 100.0 / total:4.1f}% \tMiss rate: {misses * 100.0 / total:4.1f}%")
+def main():
+    test_sources = {
+        Sharpness.SHARP: r"C:\Users\jinks\Desktop\Export\blurdet\sharp",
+        Sharpness.BLURRY: r"C:\Users\jinks\Desktop\Export\blurdet\blurry"
+    }
 
-    all_scores = [ti.scores[algo.name] for ti in test_images]
-    print("Scores = Min: {:.1f}\tMax: {:.1f}\tMean: {:.1f}".format(min(all_scores), max(all_scores),
-                                                                   sum(all_scores) / len(all_scores)))
+    test_images = []
+    for src_type, src_path in test_sources.items():
+        image_paths = sorted(list(find_images([src_path])))
+        for i_path in image_paths:
+            test_images.append(TestImage(i_path, src_type))
+    print(f"Found {len(test_images):,} test images...")
 
-    all_times = [ti.times[algo.name] for ti in test_images]
-    print(" Times = Min: {:.1f}\tMax: {:.1f}\tMean: {:.1f}".format(min(all_times), max(all_times),
-                                                                   sum(all_times) / len(all_times)))
+    algorithms = {
+        "Laplace": Algorithm("Laplace", partial(estimate_blur, just_score=True), blurriness_threshold=10),
+        "FFT": Algorithm("FFT", detect_blur_fft, blurriness_threshold=20),
+    }
+
+    print("Launching worker processess...")
+    with concurrent.futures.ProcessPoolExecutor() as executor:  # From https://stackoverflow.com/a/15143994/3915338
+        print("Submitting images to workers...")
+        futures = [executor.submit(test_an_image, image, algorithms) for image in test_images]
+        for future in concurrent.futures.as_completed(futures):
+            an_image = future.result()
+            print(an_image.result_str(algorithms))
+        print("Waiting ")
+        concurrent.futures.wait(futures)
+
+        # Un-hide exceptions
+        exceptioned = [f for f in futures if f._exception]
+        if exceptioned:
+            raise exceptioned[0]._exception
+
+        test_images = [f.result() for f in futures]
+
+
     print()
+    print(" = Algorithm results = ")
+    for algo in algorithms.values():
+        print(f" - {algo.name} - ")
+        total = len(test_images)
+        hits = len([ti for ti in test_images if ti.actual_results[algo.name] == ti.expect_result])
+        misses = total - hits
+        print(f"Hit rate: {hits * 100.0 / total:4.1f}% \tMiss rate: {misses * 100.0 / total:4.1f}%")
 
-print(" = Disagreements = ")
-for test_image in test_images:
-    sharps = map(lambda x: x == Sharpness.SHARP, test_image.actual_results.values())
-    if any(sharps) and not all(sharps):
-        print(test_image.result_str(algorithms))
+        all_scores = [ti.scores[algo.name] for ti in test_images]
+        print("Scores = Min: {:>6.1f}\tMax: {:>6.1f}\tMean: {:>6.1f}".format(min(all_scores), max(all_scores),
+                                                                       sum(all_scores) / len(all_scores)))
+
+        all_times = [ti.times[algo.name] for ti in test_images]
+        print(" Times = Min: {:>5.1f}s\tMax: {:>5.1f}s\tMean: {:>5.1f}s".format(min(all_times), max(all_times),
+                                                                       sum(all_times) / len(all_times)))
+        print()
+
+    if False:
+        print(" = Disagreements = ")
+        for test_image in test_images:
+            sharps = map(lambda x: x == Sharpness.SHARP, test_image.actual_results.values())
+            if any(sharps) and not all(sharps):
+                print(test_image.result_str(algorithms))
+
+    if True:
+        print(" = Misses = ")
+        for test_image in test_images:
+            misses = any([x != test_image.expect_result for x in test_image.actual_results.values()])
+            if misses:
+                print(test_image.result_str(algorithms))
+
+    if True:
+        print(" = Double Misses = ")
+        for test_image in test_images:
+            misses = all([x != test_image.expect_result for x in test_image.actual_results.values()])
+            if misses:
+                print(test_image.result_str(algorithms))
+
+    if True:
+        print(" = Unanimous Blurries = ")
+        for test_image in test_images:
+            blurs = all([x == Sharpness.BLURRY for x in test_image.actual_results.values()])
+            if blurs:
+                print(test_image.result_str(algorithms))
+
+
+if __name__ == '__main__':
+    main()
